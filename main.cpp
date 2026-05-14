f767938c831b487cbf3a4fe1b2aa881e2d5ce6c6
JoelSuh07 <60123817+JoelSuh07@users.noreply.github.com>
2026-05-13 21:07:22 -0700

    added arrays of vertices and edges

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
 // Implement main program.
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<int> vertex;
+
+void dijkstra(){
+  for(){
+  }
+  
+}
+
+int main(){
+
+  vector<vector<pair<int, int>>> arr[6];
+
+  arr[0] = {{1,4}, {2,9}, {4,2}};
+  arr[1] = {{2,2}, {3,1}};
+  arr[2] = {{3,7}, {5,4}};
+  arr[3] = {{5,1}};
+  arr[4] = {{2,3}, {5,8}};
+  arr[5]; // 5 does not point to anything
+  
+
+}
